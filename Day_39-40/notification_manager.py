337b681ee381e1e88cedce1bccdf0f4d7eb46301
Tanvii.k <[kawletanvi97@gmail.com]>
2023-07-16 02:29:49 +0530

    Day-40 completed

--- Day_39-40/notification_manager.py
+++ Day_39-40/notification_manager.py
@@ -1,3 +1,19 @@
+import smtplib
+
+EMAIL_PROVIDER_SMTP_ADDRESS = "smtp.gmail.com"
+
+MY_EMAIL="mailt6419@gmail.com"
+MY_PASSWORD="abcdefu123"
+
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
-    pass
+     def send_emails(self, emails, message):
+        with smtplib.SMTP(EMAIL_PROVIDER_SMTP_ADDRESS) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            for email in emails:
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}".encode('utf-8')
+                )
